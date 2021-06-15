f731c9233a95168d3d5bfb6cbf10cbaf1c45f5b5
vinay-kaushik <vinaykaushik1111@gmail.com>
2021-06-15 01:33:26 -0500

    push files

---
+++ Database/consecutiveNumbers.sql
@@ -0,0 +1,2 @@
+select distinct(l1.num) as consecutivenums from logs l1,logs l2 , logs l3 where 
+l2.id-l1.id=1 and l3.id-l2.id=1 and l1.num=l2.num and l2.num=l3.num;
---
+++ Database/custNeverOrder.sql
@@ -0,0 +1,2 @@
+select c.name as Customers from customers c 
+    where c.id not in (select o.customerid from orders o);
